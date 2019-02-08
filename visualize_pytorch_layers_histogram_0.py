d744905ca91771e9f92b7507f540f1dd6058064d
abbyYangbb <yang1489@purdue.edu>
2019-02-08 02:33:50 -0500

    update visualization

--- visualize_pytorch_layers_histogram_0.py
+++ visualize_pytorch_layers_histogram_0.py
@@ -1,5 +1,6 @@
 """
-Created on Sat Nov 18 23:12:08 2017
+This is modified from: 
+https://github.com/utkuozbulak/pytorch-cnn-visualizations/blob/master/src/cnn_layer_visualization.py
 
 @author: Utku Ozbulak - github.com/utkuozbulak
 """
@@ -20,18 +21,18 @@ class CNNLayerVisualization():
         Produces an image that minimizes the loss of a convolution
         operation for a specific layer and filter
     """
-    def __init__(self, model, selected_layers, selected_filter, img_path = "./starry_night.jpg"):
+    def __init__(self, model, img_path, selected_layers, selected_filter):
         self.model = model
         self.model.eval()
         self.selected_layers = selected_layers
         self.selected_filter = selected_filter
-        # dict for storing the output features
         self.conv_output = {}
-        # Create the folder based on the input image name to export images if not exists
-        self.output_path = img_path.split('/')[-1].split('.')[0]
+        # Create the folder to export images if not exists
+        s_a = img_path.split('/')[-1].split('.')[0] 
+        s_b = '_results'
+        self.output_path = ''.join([s_a, s_b])
         if not os.path.exists(self.output_path):
             os.mkdir(self.output_path)
-        # transform the input image based on VGG requirement
         transform = T.Compose([T.ToTensor(), 
                                T.Normalize(mean = [0.485, 0.456, 0.406], 
                                            std = [0.229, 0.224, 0.225])])
@@ -43,15 +44,10 @@ class CNNLayerVisualization():
             self.conv_output[layer] = grad_out[0, self.selected_filter]
         # Hook the selected layer
         self.model[layer].register_forward_hook(hook_function)
-    
-    # helper function to create and save histogram
+
     def create_and_save_histogram(self, data, bin_num, fname):
-        plt.figure(figsize=(5, 2.5))
-        plt.hist(data, bin_num, density=True)
-        plt.xlabel('Values')
-        plt.ylabel('Count')
-        plt.title('Histogram of {}'.format(fname.split('.')[0]))
-        plt.grid(True)
+        plt.figure(figsize=(4.5, 2.5))
+        plt.hist(data, bin_num)
         plt.savefig(os.path.join(self.output_path, fname))
 
     def visualise_layer_with_hooks(self, bin_num):
@@ -59,6 +55,9 @@ class CNNLayerVisualization():
         [self.hook_layer(i) for i in self.selected_layers]
         # get the forward stop layer
         final_layer = max(self.selected_layers)
+
+        # Generate a random image
+        random_image = np.uint8(np.random.uniform(150, 180, (224, 224, 3)))
 
         # Assign create image to a variable to move forward in the model
         x = self.image
@@ -74,20 +73,16 @@ class CNNLayerVisualization():
         # create the histograms for weights:
         for layer in self.selected_layers:
             layer_obj = self.model[layer]
-            # check if the layer has weights
             if not isinstance(layer_obj, nn.Conv2d):
+                del self.conv_output[layer]
                 continue
-            # get the weights
             weights = layer_obj.weight.data.numpy().flatten()
-            # create and output the histogram
-            self.create_and_save_histogram(weights, bin_num, 'layer{}_weights.png'.format(layer))
+            self.create_and_save_histogram(weights, bin_num, 'layer_{}_weights_histogram.png'.format(layer))
 
         # create histograms for features:
         for layer in self.conv_output:
-            # get the output of the layer
             features = self.conv_output[layer].data.numpy().flatten()
-            # create and output the histogram
-            self.create_and_save_histogram(features, bin_num, 'layer{}_features.png'.format(layer))
+            self.create_and_save_histogram(features, bin_num, 'layer_{}_features_histogram.png'.format(layer))
 
 
 
@@ -97,6 +92,7 @@ if __name__ == '__main__':
     filter_pos = 5
     # Fully connected layer is not needed
     pretrained_model = models.vgg16(pretrained=True).features
-    layer_vis = CNNLayerVisualization(pretrained_model, cnn_layers, filter_pos)
+    layer_vis = CNNLayerVisualization(pretrained_model, "./starry_night.jpg", cnn_layers, filter_pos)
+
     # Layer visualization with pytorch hooks
-    layer_vis.visualise_layer_with_hooks(bin_num = 20)
+    layer_vis.visualise_layer_with_hooks(20)
